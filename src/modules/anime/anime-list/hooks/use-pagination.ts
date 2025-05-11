85cb3bb23132f5072a4772a309903be2737e8019
ifindev <arifin.muhammad2610@Gmail.com>
2025-05-11 18:59:07 +0800

    feat: add scroll to top functionality on pagination actions

--- src/modules/anime/anime-list/hooks/use-pagination.ts
+++ src/modules/anime/anime-list/hooks/use-pagination.ts
@@ -17,28 +17,34 @@ export function usePagination({ pagination, setPage, setLimit }: UsePaginationPa
     const handleNextPage = useCallback(() => {
         if (pagination?.has_next_page) {
             setPage(pagination.current_page + 1);
+            // Scroll to the top of the page
+            window.scrollTo(0, 0);
         }
     }, [pagination, setPage]);
 
     const handlePrevPage = useCallback(() => {
         if (pagination && pagination.current_page > 1) {
             setPage(pagination.current_page - 1);
+            window.scrollTo(0, 0);
         }
     }, [pagination, setPage]);
 
     const handleFirstPage = useCallback(() => {
         setPage(1);
+        window.scrollTo(0, 0);
     }, [setPage]);
 
     const handleLastPage = useCallback(() => {
         if (pagination?.last_visible_page) {
             setPage(pagination.last_visible_page);
+            window.scrollTo(0, 0);
         }
     }, [pagination, setPage]);
 
     const handleLimitChange = useCallback(
         (newLimit: number) => {
             setLimit(newLimit);
+            window.scrollTo(0, 0);
         },
         [setLimit],
     );
